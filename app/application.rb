090eeb17436d217657f581e4dd200846d6350e78
David Hopkins <dlhopkins2@gmail.com>
2018-07-14 04:10:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,20 +2,18 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
   @@cart = []
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/items"
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
-
-
